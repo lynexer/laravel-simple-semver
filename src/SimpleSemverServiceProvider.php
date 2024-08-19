f3bfc7ff85332ac139b7cd58edfcef94de9b83c2
Kyle Graham <kylegraham05@gmail.com>
2024-08-19 14:56:32 -0700

    Add SimpleSemverServiceProvider

---
+++ src/SimpleSemverServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Lynexer\LaravelSimpleSemver;
+
+use Illuminate\Support\ServiceProvider;
+
+class SimpleSemverServiceProvider extends ServiceProvider {
+    public function register(): void {
+        // 
+    }
+    
+    public function boot(): void {
+        // 
+    }
+}
