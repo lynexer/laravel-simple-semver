7650ce76b864b15f489210417479dca4456146fe
Kyle Graham <kylegraham05@gmail.com>
2024-08-21 10:45:36 -0700

    Create simple-semver config file

--- src/SimpleSemverServiceProvider.php
+++ src/SimpleSemverServiceProvider.php
@@ -6,10 +6,10 @@ use Illuminate\Support\ServiceProvider;
 
 class SimpleSemverServiceProvider extends ServiceProvider {
     public function register(): void {
-        // 
+        $this->mergeConfigFrom(__DIR__ . '/config/simple-semver.php', 'simple-semver');
     }
     
     public function boot(): void {
-        // 
+        $this->publishes([ __DIR__ . '/config/simple-semver.php' => config_path('simple-semver.php')]);
     }
 }
---
+++ src/config/simple-semver.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Versioning File Name
+    |--------------------------------------------------------------------------
+    |
+    | The name of the file (with extension) that exists in the base
+    | path and contains the SemVer version data. This can be set to
+    | `false` to disable file checking entirely.
+    |
+    */
+
+    'file' => 'VERSION',
+];
