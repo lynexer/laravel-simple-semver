09b69f70091c407d489f1fb8e3d810231f7f67f6
Kyle Graham <kylegraham05@gmail.com>
2024-08-21 11:39:08 -0700

    Create get version method in SimpleSemver facade

--- src/SimpleSemver.php
+++ src/SimpleSemver.php
@@ -2,6 +2,36 @@
 
 namespace Lynexer\LaravelSimpleSemver;
 
+use Lynexer\LaravelSimpleSemver\Support\Exceptions\GitTagNotFound;
+use RuntimeException;
+use Symfony\Component\Process\Process;
+
 class SimpleSemver {
-    // 
+    protected string $version;
+
+    public function __construct() {
+        $this->version = $this->getVersion();
+    }
+
+    protected function getVersion(): string {
+        $version_file = static::versionFile();
+
+        if ($version_file && file_exists($version_file)) {
+            return trim(file_get_contents($version_file));
+        }
+
+        try {
+            $process = Process::fromShellCommandline('git describe --always --tags --dirty', base_path());
+            $process->mustRun();
+            $result = $process->getOutput();
+        } catch (RuntimeException $e) {
+            throw new GitTagNotFound();
+        }
+
+        return $result;
+    }
+
+    private static function versionFile(): string|bool {
+        return config('simple-semver.file') ? base_path() . '/' . config('simple-semver.file') : false;
+    }
 }
